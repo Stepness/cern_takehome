bc631388fa356e9b7a639d3f033d4a19f8961754
Stepness <stefanodinucci@gmail.com>
2023-12-20 02:10:06 +0100

    Clean code

--- solution_pie.py
+++ solution_pie.py
@@ -1,23 +1,9 @@
-def combinations(lst, n):
-    if n == 0:
-        return [[]]
-     
-    l =[]
-    for i in range(0, len(lst)):
-         
-        m = lst[i]
-        remLst = lst[i + 1:]
-         
-        remainlst_combo = combinations(remLst, n-1)
-        for p in remainlst_combo:
-             l.append([m, *p])
-           
-    return l
-
-def mountain_area(mountain) -> float:
+import itertools
+    
+def mountain_area(mountain: dict) -> float:
     return 0.5 * (mountain['right'] - mountain['left']) * mountain['height']
 
-def intersection_of_n(mountains: list):
+def intersection_of_n(mountains: list) -> float:
     total_area = 0
     rightmost_left = max(mountains, key=lambda x: x['left'])['left']
     leftmost_right = min(mountains, key=lambda x: x['right'])['right']
@@ -29,40 +15,38 @@ def intersection_of_n(mountains: list):
         return intersection_area
     return total_area
 
-def sum_of_intersections(combinations_of_n_mountains: list):
+def sum_of_intersections(combinations_of_n_mountains: list) -> float:
     sum = 0
     for mountains in combinations_of_n_mountains:
         sum += intersection_of_n(mountains)
     return sum
 
-def PIE(mountains):
+def inclusion_exclusion_principle(mountains: list) -> float:
     total_area = 0
     for current_mountain in mountains:
         total_area += mountain_area(current_mountain)
-    
+
     for index, mountain in enumerate(mountains):
-        if(index > 0):
+        if index > 0:
             exponent = pow(-1, index)
-            current_row = exponent * sum_of_intersections(combinations(mountains, index+1))
+            combinations = itertools.combinations(mountains, index+1)
+            current_row = exponent * sum_of_intersections(combinations)
             total_area += current_row
-            
+
     return total_area
-        
+
+def visible_area(mountains: list) -> float:
+    return inclusion_exclusion_principle(mountains)
 
 def main():
-    # mountains = [
-    #     {'left': 0, 'right': 6, 'height': 3},
-    #     {'left': 0, 'right':  6, 'height': 3},
-    #     {'left': 20, 'right':  30, 'height': 10},
-    # ]
     mountains = [
-        {'left': 0, 'right': 6, 'height': 3},
-        {'left': 0, 'right': 6, 'height': 3},
-        {'left': 4, 'right':  6, 'height': 1},
-    ]
-    
-    var = PIE(mountains)
-    print(var)
+    {'left': 9, 'right': 15, 'height': 3},
+    {'left': 8, 'right': 14, 'height': 3},
+    {'left': 0, 'right':  6, 'height': 3},
+]
+    res = visible_area(mountains)
+    print(res)
 
 if __name__ == "__main__":
     main()
+    
