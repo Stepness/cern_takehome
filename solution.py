ee08608950763b50b8a99c53c4c564f57b97ded0
Stepness <stefanodinucci@gmail.com>
2023-12-21 00:47:21 +0100

    Checkpoint

--- solution.py
+++ solution.py
@@ -1,15 +1,15 @@
 def _mountain_area(mountain: dict) -> float:
     return 0.5 * (mountain['right'] - mountain['left']) * mountain['height']
 
-def _in_range_mountains_combinations(mountains: list, quantity: int):
+def _summation_of_intersections_of_n_size(mountains: list, intersection_size: int):
     intersection_area = 0
     for index, mountain in enumerate(mountains): 
         temp_mountains = []
         temp_mountains.append(mountain)
-        for j, next_mountain in enumerate(mountains[index+1:index+quantity]):
+        for _, next_mountain in enumerate(mountains[index+1:index+intersection_size]):
             if(mountain['right'] >= next_mountain['left']):
                 temp_mountains.append(next_mountain)
-        if len(temp_mountains) == quantity:
+        if len(temp_mountains) == intersection_size:
             intersection_area +=_intersection_area(temp_mountains)
     return intersection_area
 
@@ -36,13 +36,12 @@ def _intersection_area(mountains: list) -> float:
 def _inclusion_exclusion_principle(mountains: list) -> float:
     total_area = 0
 
-    for mountain in mountains:
+    for index, mountain in enumerate(mountains):
         total_area += _mountain_area(mountain)
-
-    for index, _ in enumerate(mountains):
+        print(index)
         if index > 0:
             sign = pow(-1, index)
-            n_mountains_combinations = _in_range_mountains_combinations(mountains, index+1)
+            n_mountains_combinations = _summation_of_intersections_of_n_size(mountains, index+1)
             current_row = sign * n_mountains_combinations
             total_area += current_row
 
@@ -57,6 +56,7 @@ if __name__ == '__main__':
     MAXIMUM_QUANTITY_OF_MOUNTAINS = 1000
     mountain = {'left': 0, 'right': 6, 'height': 3}
     mountains = []
+    
     # mountains = [
     # {'left': 0, 'right': 10, 'height': 5},
     # {'left': 1, 'right': 11, 'height': 5},
@@ -71,15 +71,16 @@ if __name__ == '__main__':
     #     temp_mountain = {'left': 0, 'right': 6, 'height': 3}
     #     mountains.append(temp_mountain)
 
-    mountains = [
-    {'left': 9, 'right': 15, 'height': 3},
-    {'left': 8, 'right': 14, 'height': 3},
-    {'left': 0, 'right':  6, 'height': 3},
-    ]
-    # for i in range(MAXIMUM_QUANTITY_OF_MOUNTAINS):
-    #     temp_mountain = {'left': 0, 'right': 6, 'height': 3}
-    #     temp_mountain['right'] = temp_mountain['right'] + i
-    #     temp_mountain['left'] = temp_mountain['left'] + i
-    #     mountains.append(temp_mountain)
+    # mountains = [
+    # {'left': 9, 'right': 15, 'height': 3},
+    # {'left': 8, 'right': 14, 'height': 3},
+    # {'left': 0, 'right':  6, 'height': 3},
+    # ]
+    
+    for i in range(MAXIMUM_QUANTITY_OF_MOUNTAINS):
+        temp_mountain = {'left': 0, 'right': 6, 'height': 3}
+        temp_mountain['right'] = temp_mountain['right'] + i
+        temp_mountain['left'] = temp_mountain['left'] + i
+        mountains.append(temp_mountain)
     
     print(visible_area(mountains))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,5 @@
-
-    mountains = [
-    {'left': 9, 'right': 15, 'height': 3},
-    {'left': 8, 'right': 14, 'height': 3},
-    {'left': 0, 'right':  6, 'height': 3},
-]
+      if index > 0:
+            sign = pow(-1, index)
+            n_mountains_combinations = _summation_of_intersections_of_n_size(mountains, index+1)
+            current_row = sign * n_mountains_combinations
+            total_area += current_row
