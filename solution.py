97d81c60dda83fa9ecb585aab2744fe44ac0ab8e
Stepness <stefanodinucci@gmail.com>
2023-12-20 19:19:03 +0100

    Checkpoint

--- solution.py
+++ solution.py
@@ -28,7 +28,6 @@ def _inclusion_exclusion_principle(mountains: list) -> float:
 
     for index, _ in enumerate(mountains):
         if index > 0:
-            print(index)
             sign = pow(-1, index)
             n_mountains_combinations = itertools.combinations(mountains, index+1)
             current_row = sign * _intersections_summation(n_mountains_combinations)
