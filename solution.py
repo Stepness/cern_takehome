c6871be22e75dfdd387e24025c46b0939ec152aa
Stepness <stefanodinucci@gmail.com>
2023-12-20 19:15:17 +0100

    Tests done

--- solution.py
+++ solution.py
@@ -1,38 +1,40 @@
-def mountain_area(mountain) -> float:
-    return 0.5 * (mountain['right'] - mountain['left']) * mountain['height']
+import itertools
     
+def _mountain_area(mountain: dict) -> float:
+    return 0.5 * (mountain['right'] - mountain['left']) * mountain['height']
+
+def _areas_intersection(mountains: list) -> float:
+    rightmost_left = max(mountains, key=lambda x: x['left'])['left']
+    leftmost_right = min(mountains, key=lambda x: x['right'])['right']
+    
+    intersection_base = leftmost_right - rightmost_left
+    
+    if intersection_base > 0:
+        intersection_height = 0.5 * intersection_base
+        intersection_area = 0.5 * intersection_base * intersection_height
+        return intersection_area
+    return 0
+
+def _intersections_summation(mountains_combinations: list) -> float:
+    summation = 0
+    for mountains_combination in mountains_combinations:
+        summation += _areas_intersection(mountains_combination)
+    return summation
+
+def _inclusion_exclusion_principle(mountains: list) -> float:
+    total_area = 0
+    for mountain in mountains:
+        total_area += _mountain_area(mountain)
+
+    for index, _ in enumerate(mountains):
+        if index > 0:
+            print(index)
+            sign = pow(-1, index)
+            n_mountains_combinations = itertools.combinations(mountains, index+1)
+            current_row = sign * _intersections_summation(n_mountains_combinations)
+            total_area += current_row
+
+    return total_area
 
 def visible_area(mountains: list) -> float:
-    visible_area = 0
-    for index, current_mountain in enumerate(mountains):
-        current_mountain_area = mountain_area(current_mountain)
-        
-        for previous_mountain in mountains[:index]:
-            rightmost_left = max(current_mountain['left'], previous_mountain['left'])
-            leftmost_right = min(current_mountain['right'], previous_mountain['right'])
-            intersection_base = leftmost_right - rightmost_left
-            if(intersection_base > 0):
-                intersection_height = 0.5 * intersection_base
-                intersection_area = 0.5 * intersection_base * intersection_height
-                
-                current_mountain_area -= intersection_area
-            
-        visible_area += current_mountain_area
-    return visible_area
-            
-
-
-
-def main():
-    mountains = [
-        {'left': 9, 'right': 15, 'height': 3},
-        {'left': 8, 'right': 14, 'height': 3},
-        {'left': 0, 'right':  6, 'height': 3},
-    ]
-
-    
-    result = visible_area(mountains)
-    print("Visible Area:", result)
-
-if __name__ == "__main__":
-    main()
+    return _inclusion_exclusion_principle(mountains)
--- test.py
+++ test.py
@@ -1,20 +1,59 @@
 import unittest
+import solution as sut
 
-class TestStringMethods(unittest.TestCase):
+class TestMountainsVisibleArea(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_given_non_overlapping_mountains_when_calculating_visible_area_then_return_sum_of_all_areas(self):
+        mountains = [
+            {'left': 0, 'right': 1000, 'height': 500},
+            {'left': 1000, 'right':  2000, 'height': 500},
+        ]
+        
+        expected_area = 2 * 250_000
+        
+        area = sut.visible_area(mountains)
+        self.assertEqual(area, expected_area)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_given_mountains_overlapping_in_same_spot_when_calculating_visible_area_then_return_biggest_area(self):
+        mountains = [
+            {'left': 9, 'right': 15, 'height': 3},
+            {'left': 9, 'right': 15, 'height': 3},
+            {'left': 9, 'right': 15, 'height': 3},
+            {'left': 11, 'right': 15, 'height': 2},
+            {'left': 11, 'right': 15, 'height': 2},
+            {'left': 11, 'right': 15, 'height': 2},
+        ]
+        
+        expected_area = 9.0
+        
+        area = sut.visible_area(mountains)
+        
+        self.assertEqual(area, expected_area)
+        
+    def test_given_mountains_partially_overlapping_when_calculating_visible_area_then_return_expected_value(self):
+        inputs = [
+            ([
+                {'left': 0, 'right': 6, 'height': 3},
+                {'left': 4, 'right': 6, 'height': 1},
+                {'left': 4, 'right': 10, 'height': 3},], 17.0),
+            ([
+                {'left': 9, 'right': 15, 'height': 3},
+                {'left': 8, 'right': 14, 'height': 3},
+                {'left': 0, 'right':  6, 'height': 3},], 20.75),
+        ]
+        
+        
+        for mountains, expected_area in inputs:
+            mountain_area = sut.visible_area(mountains)
+            self.assertEqual(mountain_area, expected_area)
+    
+    def test_given_no_mountains_when_calculating_visible_area_then_return_zero(self):
+        expected_area = 0.0
+        
+        area = sut.visible_area({})
+        
+        self.assertEqual(area, expected_area)
+    
 
 if __name__ == '__main__':
     unittest.main()
